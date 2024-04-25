f7556063559b7d8550c530b1d84f9273f219fd35
Hadi Sheikhi <hadsheikhi@gmail.com>
2024-04-25 16:09:18 -0600

    dockerize chat and integrations to django

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,4 +14,6 @@ python manage.py migrate
 echo 'Collecting static files...'
 python manage.py collectstatic --no-input
 
-python manage.py runserver 0.0.0.0:8000
+# python manage.py runserver 0.0.0.0:8000
+
+exec "$@"
