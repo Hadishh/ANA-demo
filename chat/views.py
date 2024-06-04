23ab40f29b7ae4e6419851ab76c268bb7ab92960
Hadi Sheikhi <hadsheikhi@gmail.com>
2024-06-04 13:28:41 -0600

    make server separate

--- chat/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.views.generic import TemplateView
-
-class ChatView(TemplateView):
-    template_name: str = "chat/chat.html"
---
+++ server/config/settings/development.py
@@ -0,0 +1,8 @@
+from .base import *
+import os
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "../", "mediafiles")
+
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "../", "staticfiles")
