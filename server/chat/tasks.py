b0393e94c0422cafa8cba4693ae63fd0d9016c03
Hadi Sheikhi <hadsheikhi@gmail.com>
2024-06-05 21:36:04 -0600

    add messages database info

--- server/chat/tasks.py
+++ server/chat/tasks.py
@@ -4,10 +4,11 @@ from asgiref.sync import async_to_sync
 from celery import shared_task
 from channels.layers import get_channel_layer
 from core.ana import ChatBot
+from .models import Message
 channel_layer = get_channel_layer()
 
 @shared_task
-def get_response(channel_name, input_data):
+def get_response(channel_name, input_data, user):
     # conversation = Conversation.objects.all().values()
 
     # if (len(Dictionary.objects.all()) == 0):
@@ -19,10 +20,14 @@ def get_response(channel_name, input_data):
     # # chatbot = ChatBot(conversation=conversation, dictionary=dictionary)
     # answer = chatbot.answer(input_data['text'])
 
+    answer = "HI HOW CAN I HELP YOU?"
     async_to_sync(channel_layer.send)(
         channel_name,
         {
             "type": "chat.message",
-            "text": {"msg": "HI HOW CAN I HELP YOU?", "source": "bot"},
+            "text": {"msg": answer, "source": "bot"},
         },
     )
+
+    new_message = Message.objects.create(owner=user, text=answer, source="bot")
+    new_message.save()
